ebfc2f01a4c29fc23306ca66092dbe06659bcb5e
camilo-gc <camilogarcia.dev@gmail.com>
2024-04-06 17:42:19 -0500

    feat: hu01-registrar tecnologias

---
+++ src/main/java/com/example/bootcampmicroservice/application/handler/TechnologyHandlerImpl.java
@@ -0,0 +1,31 @@
+package com.example.bootcampmicroservice.application.handler;
+
+import com.example.bootcampmicroservice.application.dto.TechnologyRequestDto;
+import com.example.bootcampmicroservice.application.mapper.*;
+import com.example.bootcampmicroservice.domain.api.ITechnologyServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TechnologyHandlerImpl implements ITechnologyHandler {
+
+    private final ITechnologyServicePort technologyServicePort;
+    private final TechnologyRequestMapper technologyRequestMapper;
+//    private final TechnologyResponseMapper technologyResponseMapper;
+
+    @Override
+    public void saveTechnology(TechnologyRequestDto technologyRequestDto) {
+        technologyServicePort.saveTechnology(
+                technologyRequestMapper.toModel(technologyRequestDto)
+        );
+    }
+
+//    @Override
+//    public List<TechnologyResponseDto> getAllTechnologies() {
+//        return technologyResponseMapper.toResponseList(technologyServicePort.getAllTechnologies());
+//    }
+
+}
---
+++ src/main/java/com/example/bootcampmicroservice/infrastructure/out/jpa/adapter/TechnologyJpaAdapter.java
@@ -0,0 +1,35 @@
+package com.example.bootcampmicroservice.infrastructure.out.jpa.adapter;
+
+import com.example.bootcampmicroservice.domain.model.TechnologyModel;
+import com.example.bootcampmicroservice.domain.spi.ITechnologyPersistencePort;
+import com.example.bootcampmicroservice.infrastructure.exception.TechnologyNameAlreadyExistsException;
+import com.example.bootcampmicroservice.infrastructure.out.jpa.mapper.TechnologyMapper;
+import com.example.bootcampmicroservice.infrastructure.out.jpa.repository.ITechnologyRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TechnologyJpaAdapter implements ITechnologyPersistencePort {
+
+    private final ITechnologyRepository technologyRepository;
+    private final TechnologyMapper technologyMapper;
+
+    @Override
+    public TechnologyModel saveTechnology(TechnologyModel technologyModel) {
+
+        TechnologyModel model = technologyMapper.toModel(technologyRepository.findByName(technologyModel.getName()));
+        if (model != null) throw new TechnologyNameAlreadyExistsException();
+        return technologyMapper.toModel(
+                technologyRepository.save( technologyMapper.toEntity(technologyModel) )
+        );
+    }
+
+//    @Override
+//    public List<TechnologyModel> getAllTechnologies() {
+//        List<TechnologyEntity> technologyEntityList = technologyRepository.findAll();
+//        if (technologyEntityList.isEmpty()) {
+//            throw new NoDataFoundException();
+//        }
+//        return technologyMapper.toTechnologyList(technologyEntityList);
+//    }
+
+}
